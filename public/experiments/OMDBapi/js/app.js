0bad389be63a56fcf6876aec8556d89a63537b84
anvitasurapaneni <surapaneni.a@husky.neu.edu>
2016-02-09 17:46:06 -0500

    experimenting with omdb api

--- public/experiments/OMDBapi/js/app.js
+++ public/experiments/OMDBapi/js/app.js
@@ -1,21 +1,68 @@
 /**
  * Created by anvitasurapaneni on 2/3/16.
  */
-alert("hello");
-(function()
-{$(init);
-var $moviename;
-    var $select;
-    function  init(){
-        var $moviename = $("#moviename");
-        var $select = $("#select");
 
-        $select.click(searchmovie);
+(function(){
+    $(init);
+    var $MovieName;
+    var $SearchMovieTitle;
+    var $tbody;
+    var $SearchUrl ="http://www.omdbapi.com/?s=TITLE&page=PAGE";
+
+    function init(){
+         $MovieName = $("#MovieName");
+         $SearchMovieTitle = $("#SearchMovieTitle");
+        $SearchMovieTitle.click(searchMovie);
+        $tbody = $("#searchResults tbody");
+
     }
 
-    function searchmovie(){
-        var mt= $moviename;
-        alert("helo" +mt);
-    });
+    function searchMovie(){
+
+        var mn = $MovieName.val();
+     //   var name = anvita;
+
+        var url = $SearchUrl.replace("TITLE", mn)
+            .replace("PAGE", 1);
+     //   alert("url:" + url)
+        $.ajax({
+            url: url,
+            success: renderMovieList
+        });
+            }
+
+    function renderMovieList(response){
+        console.log(response);
+        var totalResults = Response.totalResults;
+        var movies = response.Search;
+        for(var m=0; m<movies.length; m++)
+        {
+            var movie = movies[m];
+
+            var Title = movie.Title;
+            var imdbID = movie.imdbID;
+            var Poster = movie.Poster;
+
+            console.log(Title);
+            var $tr =$("<tr>");
+
+            var $img = $("<img>").attr("src", Poster)
+                .addClass("poster");
+
+            var $td =$("<td>");
+            $td.append($img);
+            $tr.append($td);
+
+             $td =$("<td>")
+                .append(Title);
+            $tr.append($td);
+
+            $td =$("<td>")
+                .append(imdbID);
+            $tr.append($td);
+
+            $tbody.append($tr);
+        }
+    }
 
 })();
