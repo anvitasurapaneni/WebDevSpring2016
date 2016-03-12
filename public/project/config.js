946e2677d98059cc90db0ffb855c40fa4a57a8f6
anvitasurapaneni <surapaneni.a@husky.neu.edu>
2016-03-11 23:53:03 -0500

    project: implemented few widgets and notebook module

--- public/project/config.js
+++ public/project/config.js
@@ -21,6 +21,14 @@
                 templateUrl: "views/widgets/text/text.view.html",
                 controller: "TextEditorController"
             })
+            .when("/map", {
+                templateUrl: "views/widgets/googlemap/map.view.html",
+                controller: "MapController"
+            })
+            .when("/note", {
+                templateUrl: "views/widgets/note/note.view.html",
+                controller: "NoteController"
+            })
             .when("/todo", {
                 templateUrl: "views/widgets/todoWidget/todoWidget.view.html",
                 controller: "toDoController"
@@ -36,10 +44,6 @@
             .when("/notebook", {
                 templateUrl: "views/widgets/notebook/notebook.view.html",
                 controller: "noteBookController"
-            })
-            .when("/map", {
-                templateUrl: "views/widgets/googlemap/map.view.html",
-                controller: "MapController"
             })
             .when("/login", {
                 templateUrl: "views/users/login.view.html",
