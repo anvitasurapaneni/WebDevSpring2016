d95b00d751be77fc8a05b459537ed31bcaff46ce
anvitasurapaneni <surapaneni.a@husky.neu.edu>
2016-03-23 20:33:32 -0400

    general push

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
