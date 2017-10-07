075a9a639d97aea386af678941640b0b35452c29
anvitasurapaneni <surapaneni.a@husky.neu.edu>
2017-10-06 23:09:34 -0400

    shifting to heroku

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ var passport = require('passport');
 var connectionString = 'mongodb://127.0.0.1:27017/cs5610fall2015exmpl1';
 
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
+if(process.env.MONGODB_URI) {
+
+    connectionString = process.env.MONGODB_URI;
 }
 
 
